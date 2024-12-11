c8ad2bdc42ff135ca092ffa86d8d20fbbc1673d4
Kaleb Eyasu <kalu4mom@gmail.com>
2024-12-11 06:46:21 +0300

    Update CatItem.js

--- models/CatItem.js
+++ models/CatItem.js
@@ -1,12 +1,11 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../db/dbConnect.js'; // Adjust path if necessary
-import Subcat from './Subcat.js'; // Import Subcat model
 
 const CatItem = sequelize.define('CatItem', {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true, // Ensures CatItem names are unique
+    unique: true,
     validate: {
       notEmpty: {
         msg: 'CatItem name cannot be empty',
@@ -14,18 +13,12 @@ const CatItem = sequelize.define('CatItem', {
     },
   },
   image: {
-    type: DataTypes.STRING, // Store the image URL/path or filename
+    type: DataTypes.STRING,
     allowNull: true,
   },
 }, {
-  timestamps: true, // Sequelize will automatically add 'createdAt' and 'updatedAt'
-  tableName: 'cat_items', // Define custom table name if needed
-});
-
-// Association: CatItem belongs to Subcat
-CatItem.belongsTo(Subcat, {
-  foreignKey: 'subcatId', // Add `subcatId` column in `CatItem` table
-  as: 'subcat', // Alias for accessing the Subcat relation
+  timestamps: true,
+  tableName: 'cat_items',
 });
 
 export default CatItem;
