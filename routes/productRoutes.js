e27f9e7a8384d831bca032417dce49f3b1b52eec
Kaleb Eyasu <kalu4mom@gmail.com>
2024-12-11 21:19:20 +0300

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,30 +1,27 @@
-// routes/productRoutes.js
 import express from 'express';
-import { 
-  addProduct, 
-  getAllProducts, 
-  getProductById, 
-  updateProduct, 
-  deleteProduct 
-} from '../controllers/productController.js'; // Adjust the path as necessary
-import adminAuth from '../middlewares/adminMiddleware.js'; // Adjust the path as necessary
-import { authenticateSeller } from '../middlewares/sellerMiddleware.js'; // Adjust the path as necessary
+import {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} from '../controllers/AddProductController.js';
 
 const router = express.Router();
 
-// Route to add a new product (protected by adminAuth and authenticateSeller)
-router.post('/add', adminAuth, addProduct);
+// Route to create a product
+router.post('/create', createProduct);
 
-// Route to get all products (could be public)
+// Route to get all products
 router.get('/', getAllProducts);
 
-// Route to get a single product by ID (could be public)
+// Route to get a product by ID
 router.get('/:id', getProductById);
 
-// Route to update a product (protected by adminAuth)
-router.put('/products/:id', adminAuth, updateProduct);
+// Route to update a product by ID
+router.put('/update/:id', updateProduct);
 
-// Route to delete a product (protected by adminAuth)
-router.delete('/products/:id', adminAuth, deleteProduct);
+// Route to delete a product by ID
+router.delete('/delete/:id', deleteProduct);
 
 export default router;
