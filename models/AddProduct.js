84861a596dc056ab7f5aaf2c798b0279aa225c28
Kaleb Eyasu <kalu4mom@gmail.com>
2024-12-12 09:56:09 +0300

    Update AddProduct.js

--- models/AddProduct.js
+++ models/AddProduct.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../config/database.js'; // Your Sequelize connection
+import sequelize from '../db/dbConnect.js';  // Your Sequelize connection
 
 const AddProduct = sequelize.define('AddProduct', {
   id: {
