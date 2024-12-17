1827581614dfedec48154b83ef0aec9a0e059189
Kaleb Eyasu <kalu4mom@gmail.com>
2024-12-17 15:12:46 +0300

    Create DeliveryBoy.js

---
+++ models/DeliveryBoy.js
@@ -0,0 +1,43 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+const DeliveryBoy = sequelize.define(
+  'DeliveryBoy',
+  {
+    full_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Full name is required.' },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: { msg: 'Please enter a valid email address.' },
+      },
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Location is required.' },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Password is required.' },
+      },
+    },
+  },
+  {
+    tableName: 'delivery_boys',
+    timestamps: true,
+  }
+);
+
+export default DeliveryBoy;
