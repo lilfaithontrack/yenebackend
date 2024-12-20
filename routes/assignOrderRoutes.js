361c490c45dea6372e7a1becdb41de9f65a3e642
Kaleb Eyasu <kalu4mom@gmail.com>
2024-12-20 14:59:01 +0300

    Update assignOrderRoutes.js

--- routes/assignOrderRoutes.js
+++ routes/assignOrderRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { assignOrderToShopperAndDelivery, getOrderAssignments, updateAssignmentStatus } from '../controllers/assignOrderController';
+import { assignOrderToShopperAndDelivery, getOrderAssignments, updateAssignmentStatus } from '../controllers/assignOrderController.js';
 
 const router = express.Router();
 
