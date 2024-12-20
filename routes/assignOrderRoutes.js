8603c836a94b2cad3e7e2706c1dfc00d5fe47bc8
Kaleb Eyasu <kalu4mom@gmail.com>
2024-12-20 11:30:37 +0300

    Create assignOrderRoutes.js

---
+++ routes/assignOrderRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { assignOrderToShopperAndDelivery, getOrderAssignments, updateAssignmentStatus } from '../controllers/assignOrderController';
+
+const router = express.Router();
+
+// Route to assign an order to a shopper and delivery boy
+router.put('/:order_id/assign', assignOrderToShopperAndDelivery);
+
+// Route to get the assignments for an order
+router.get('/:order_id/assignments', getOrderAssignments);
+
+// Route to update the status of an assignment
+router.put('/assignment/:assignment_id/status', updateAssignmentStatus);
+
+export default router;
