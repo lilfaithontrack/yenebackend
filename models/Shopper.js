ff2554604451058e391e15ca97d50429946400c0
Kaleb Eyasu <kalu4mom@gmail.com>
2024-12-23 16:56:06 +0300

    Update Shopper.js

--- models/Shopper.js
+++ models/Shopper.js
@@ -51,7 +51,7 @@ const Shopper = sequelize.define(
   }
 );
 
-Shopper.hasMany(Assignment, { foreignKey: 'shopper_id' });
+Shopper.hasMany(AssignOrder, { foreignKey: 'shopper_id' });
 
 
 export default Shopper;
