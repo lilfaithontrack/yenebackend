e66dd98303d5e00c671327f5f5f1417413f96f3b
Kaleb Eyasu <kalu4mom@gmail.com>
2024-12-28 14:25:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ const app = express();
 // Middleware
 app.use(express.json()); // Parse JSON
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://mobile.yeniesuq.com' ,'https://mobadmin.yeniesuq.com'], // Update to match your frontend URLs
+  origin: ['http://localhost:3000', 'https://mobile.yeniesuq.com' ,'https://mobadmin.yeniesuq.com','https://mobapp.piazdelivery.com'], // Update to match your frontend URLs
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
 }));
