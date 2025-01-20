96eafed3b1dc3a78fb1d0bb879d9901f32543197
Kaleb Eyasu <kalu4mom@gmail.com>
2025-01-20 10:52:25 +0300

    Update Chat.js

--- models/Chat.js
+++ models/Chat.js
@@ -1,1 +1,39 @@
 
+import { DataTypes } from 'sequelize';
+import sequelize from '../db/dbConnect.js';
+
+const Chat = sequelize.define(
+  'Chat',
+  {
+    sender_id: {
+      type: DataTypes.STRING, // ID of the sender (admin, shopper, or delivery personnel)
+      allowNull: false,
+    },
+    receiver_id: {
+      type: DataTypes.STRING, // ID of the receiver
+      allowNull: false,
+    },
+    sender_role: {
+      type: DataTypes.ENUM('admin', 'shopper', 'delivery'), // Role of the sender
+      allowNull: false,
+    },
+    receiver_role: {
+      type: DataTypes.ENUM('admin', 'shopper', 'delivery'), // Role of the receiver
+      allowNull: false,
+    },
+    message: {
+      type: DataTypes.TEXT, // The message content
+      allowNull: false,
+    },
+    is_read: {
+      type: DataTypes.BOOLEAN, // Indicates if the message has been read
+      defaultValue: false,
+    },
+  },
+  {
+    tableName: 'Chats', // Name of the table
+    timestamps: true, // Automatically add createdAt and updatedAt
+  }
+);
+
+export default Chat;
