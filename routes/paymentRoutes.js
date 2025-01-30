a3bc715ef86fe59006dbfd89d304f482d875e46b
Kaleb Eyasu <kalu4mom@gmail.com>
2025-01-30 07:28:44 +0300

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -4,7 +4,8 @@ import {
   updatePaymentStatus, 
   sendOrderToShopperAndDelivery, 
   getOrderHistory, 
-  getAllOrders 
+  getAllOrders ,
+  getPaymentOrderById 
 } from '../controllers/paymentController.js';
 
 const router = express.Router();
@@ -20,6 +21,8 @@ router.get('/orders/history', getOrderHistory);
 
 // Route to fetch all orders
 router.get('/orders', getAllOrders);
+//route to fetch the payment order based on the id 
+router.get('/orders/:payment_id', getPaymentOrderById);
 
 // Route to send order to shopper and delivery (approve and assign)
 router.put('/:payment_id/send-order', sendOrderToShopperAndDelivery);
