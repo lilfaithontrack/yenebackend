fbbc2e219cdb254061fb9ec9f1c51ec95ad6f6ef
Kaleb Eyasu <kalu4mom@gmail.com>
2025-01-30 07:25:26 +0300

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -192,6 +192,23 @@ const getOrderHistory = async (req, res) => {
     res.status(500).json({ message: 'Internal server error.' });
   }
 };
+//get payment orders by their id 
+const getPaymentOrderById = async (req, res) => {
+  try {
+    const { payment_id } = req.params;
+
+    const payment = await Payment.findByPk(payment_id);
+    if (!payment) {
+      return res.status(404).json({ message: 'Payment order not found.' });
+    }
+
+    return res.status(200).json({ message: 'Payment order retrieved successfully.', payment });
+  } catch (error) {
+    console.error('Error fetching payment order:', error);
+    res.status(500).json({ message: 'Internal server error.' });
+  }
+};
+
 
 // Fetch all orders
 const getAllOrders = async (req, res) => {
@@ -211,4 +228,4 @@ const getAllOrders = async (req, res) => {
   }
 };
 
-export { createPayment, updatePaymentStatus, sendOrderToShopperAndDelivery, getOrderHistory, getAllOrders };
+export { createPayment, updatePaymentStatus, sendOrderToShopperAndDelivery, getOrderHistory, getAllOrders ,getPaymentOrderById };
