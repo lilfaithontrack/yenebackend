e66f7278419897a215a0e046e830d7eac3b68af7
Kaleb Eyasu <kalu4mom@gmail.com>
2025-03-15 03:20:08 +0300

    Update sellerProductsRoutes.js

--- routes/sellerProductsRoutes.js
+++ routes/sellerProductsRoutes.js
@@ -6,6 +6,7 @@ import {
   getPendingSellerProducts, 
   deleteSellerProduct,
   updateSellerProduct,
+  updateSellerProductStatus,
    getAllSellerProducts,
   upload 
 } from '../controllers/sellerProductController.js';
@@ -17,6 +18,9 @@ router.post('/add', upload.array('image', 10), createSellerProduct);
 
 // Approve a Seller Product (Admin Only)
 router.put('/approve/:id', approveSellerProduct);
+// update Seller Product status 
+
+router.put('/updatestatus/:id', updateSellerProductStatus);
 
 //get all the seller products 
 
