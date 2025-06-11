1cbca1a1c0bed6ef5590f9713d568932a6be34fe
Kaleb Eyasu <kalu4mom@gmail.com>
2025-06-11 06:59:25 +0300

    Update Shopper.js

--- models/Shopper.js
+++ models/Shopper.js
@@ -1,5 +1,5 @@
-import { Model, DataTypes } from 'sequelize';
-import sequelize from '../db/dbConnect.js'; // Adjust the path to your sequelize instance
+import { DataTypes } from 'sequelize';
+import sequelize from '../db/dbConnect.js';
 
 const Shopper = sequelize.define(
   'Shopper',
@@ -17,7 +17,7 @@ const Shopper = sequelize.define(
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,  // Ensures email is unique
+      unique: true,
     },
     password: {
       type: DataTypes.STRING,
@@ -31,27 +31,14 @@ const Shopper = sequelize.define(
       type: DataTypes.DOUBLE,
       allowNull: false,
     },
-    created_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      allowNull: true,
-    },
-     updatedAt: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      onUpdate: DataTypes.NOW,
-      allowNull: true,
-    },
   },
   {
     sequelize,
     modelName: 'Shopper',
-    tableName: 'shoppers', // Ensuring the table name is correct
-    timestamps: false, // We will manually handle created_at and updated_at columns
+    tableName: 'shoppers',
+    timestamps: true, // Automatically adds createdAt and updatedAt
+    underscored: true, // Creates columns as created_at, updated_at
   }
 );
-
-
-
 
 export default Shopper;
