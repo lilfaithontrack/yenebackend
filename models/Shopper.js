c8f33a39f2035b81e692e4dcf35fcc13b967e3e2
Kaleb Eyasu <kalu4mom@gmail.com>
2025-09-19 13:37:41 +0300

    Add files via upload

--- models/Shopper.js
+++ models/Shopper.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../db/dbConnect.js';
+import {sequelize} from '../config/dbConnect.js';
 
 const Shopper = sequelize.define(
   'Shopper',
